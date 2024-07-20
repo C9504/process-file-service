de051ab6498974f00b80b79e9e81c2cfea7e1652
unknown <elperezanthony@gmail.com>
2024-07-19 20:56:21 -0500

    Fixed bugs

--- src/main/java/com/georeference/batch/GeoreferenceRecordProcessor.java
+++ src/main/java/com/georeference/batch/GeoreferenceRecordProcessor.java
@@ -4,6 +4,7 @@ import com.georeference.appregca.entities.Department;
 import com.georeference.appregca.entities.Municipality;
 import com.georeference.appregca.repositories.DepartmentRepository;
 import com.georeference.appregca.repositories.MunicipalityRepository;
+import com.georeference.batch.validations.GeoreferenceValidator;
 import com.georeference.process.entities.GeoreferenceRecord;
 import com.georeference.process.entities.GeoreferenceRecordFail;
 import com.georeference.process.entities.GeoreferenceRequest;
@@ -15,28 +16,26 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @JobScope
 @Component
 public class GeoreferenceRecordProcessor implements ItemProcessor<GeoreferenceRecord, GeoreferenceRecord> {
 
-    private static final String DEPARTMENT_DANE_CODE = "Código DANE Departamento";
-    private static final String MUNICIPALITY_DANE_CODE = "Código DANE Municipio";
-    private static final String FARMER_NAME = "Nombre del Caficultor";
-
     private final Long requestId;
     private int row = 1;
 
+    private final GeoreferenceValidator validator;
     private final GeoreferenceRequestService georeferenceRequestService;
-
     private final DepartmentRepository departmentRepository;
     private final MunicipalityRepository municipalityRepository;
 
-    private final List<GeoreferenceRecordFail> errors = new ArrayList<>();
+    private List<GeoreferenceRecordFail> errors = new ArrayList<>();
 
-    GeoreferenceRecordProcessor(@Value("#{jobParameters['requestId']}") Long requestId, GeoreferenceRequestService georeferenceRequestService, DepartmentRepository departmentRepository, MunicipalityRepository municipalityRepository) {
+    GeoreferenceRecordProcessor(@Value("#{jobParameters['requestId']}") Long requestId, GeoreferenceValidator validator, GeoreferenceRequestService georeferenceRequestService, DepartmentRepository departmentRepository, MunicipalityRepository municipalityRepository) {
         this.georeferenceRequestService = georeferenceRequestService;
         this.requestId = requestId;
+        this.validator = validator;
         this.departmentRepository = departmentRepository;
         this.municipalityRepository = municipalityRepository;
     }
@@ -48,62 +47,12 @@ public class GeoreferenceRecordProcessor implements ItemProcessor<GeoreferenceRe
         Municipality municipality = municipalityRepository.findByTxCodeDane(entity.getMunicipalityCode());
         row++;
         entity.setGeoreferenceRequest(georeferenceRequest);
-        validateFields(entity, georeferenceRequest, department, municipality);
+        this.errors = validator.validateFields(entity, georeferenceRequest, department, municipality, row);
+        if (!this.errors.isEmpty()) {
+            Objects.requireNonNull(georeferenceRequest).setStatus("CON ERROR");
+            georeferenceRequestService.updateGeoreferenceRequest(georeferenceRequest);
+        }
         return entity;
-    }
-
-    private void addError(GeoreferenceRequest georeferenceRequest, String columnName, Integer rowNumber, String errorMessage) {
-        GeoreferenceRecordFail rf = new GeoreferenceRecordFail();
-        rf.setGeoreferenceRequest(georeferenceRequest);
-        rf.setColumnName(columnName);
-        rf.setRowNumber(rowNumber);
-        rf.setErrorMessage(errorMessage);
-        errors.add(rf);
-    }
-
-    private void validateFields(GeoreferenceRecord entity, GeoreferenceRequest georeferenceRequest, Department department, Municipality municipality) {
-        if (entity.getFarmerName().equalsIgnoreCase("")) {
-            addError(georeferenceRequest, FARMER_NAME, row, "EL NOMBRE CAFICULTOR ES REQUERIDO");
-        }
-        if (entity.getFarmerName().length() > 200) {
-            addError(georeferenceRequest, FARMER_NAME, row, "EL NOMBRE CAFICULTOR EXCEDE LOS 200 CARACTERES");
-        }
-        if (entity.getDocumentType().equalsIgnoreCase("") || entity.getDocumentType().length() > 4) {
-            addError(georeferenceRequest, "Tipo de Documento", row, "EL TIPO DE DOCUMENTO EXCEDE LA LONGITUD");
-        }
-        if (entity.getDocumentNumber().toString().length() > 15) {
-            addError(georeferenceRequest, "Numero de Documento", row, "EL NÚMERO DE DOCUMENTO EXCEDE LA LONGITUD");
-        }
-        if (entity.getFarmName().length() > 200) {
-            addError(georeferenceRequest, "Nombre de la Finca", row, "EL NOMBRE DE LA FINCA EXCEDE LOS 200 CARACTERES");
-        }
-        if (entity.getCultivationArea() == 0) {
-            addError(georeferenceRequest, "Área de Cultivo", row, "EL ÁREA DE CULTIVO NO CUMPLE CON EL FORMATO");
-        }
-        if (entity.getDepartmentCode().equalsIgnoreCase("")) {
-            addError(georeferenceRequest, DEPARTMENT_DANE_CODE, row, "CÓDIGO DEPARTAMENTO ES REQUERIDO");
-        }
-        if (entity.getDepartmentCode().length() > 3) {
-            addError(georeferenceRequest, DEPARTMENT_DANE_CODE, row, "CÓDIGO DEPARTAMENTO EXCEDE LA LONGITUD");
-        }
-        if (department != null && !entity.getDepartmentCode().equals(department.getTxCodeDane())) {
-            addError(georeferenceRequest, DEPARTMENT_DANE_CODE, row, "EL CÓDIGO DANE DEL DEPARTAMENTO NO EXISTE");
-        }
-        if (entity.getDepartmentName().length() > 100) {
-            addError(georeferenceRequest, "Departamento", row, "EL NOMBRE DEL DEPARTAMENTO EXCEDE LOS 100 CARACTERES");
-        }
-        if (entity.getMunicipalityCode().equalsIgnoreCase("")) {
-            addError(georeferenceRequest, MUNICIPALITY_DANE_CODE, row, "CÓDIGO DEPARTAMENTO ES REQUERIDO");
-        }
-        if (entity.getMunicipalityCode().length() > 2) {
-            addError(georeferenceRequest, MUNICIPALITY_DANE_CODE, row, "CÓDIGO DEPARTAMENTO EXCEDE LA LONGITUD");
-        }
-        if (municipality != null && !entity.getMunicipalityCode().equals(municipality.getTxCodeDane())) {
-            addError(georeferenceRequest, MUNICIPALITY_DANE_CODE, row, "EL CÓDIGO DANE DEL MUNICIPIO NO EXISTE");
-        }
-        if (entity.getMunicipalityName().length() > 100) {
-            addError(georeferenceRequest, "Municipio", row, "EL NOMBRE DEL MUNICIPIO EXCEDE LOS 100 CARACTERES");
-        }
     }
 
     public List<GeoreferenceRecordFail> getErrorList() {
---
+++ src/main/java/com/georeference/batch/validations/GeoreferenceValidator.java
@@ -0,0 +1,113 @@
+package com.georeference.batch.validations;
+
+import com.georeference.appregca.entities.Department;
+import com.georeference.appregca.entities.Municipality;
+import com.georeference.process.entities.GeoreferenceRecord;
+import com.georeference.process.entities.GeoreferenceRecordFail;
+import com.georeference.process.entities.GeoreferenceRequest;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class GeoreferenceValidator {
+
+    private static final String DEPARTMENT_DANE_CODE = "Código DANE Departamento";
+    private static final String MUNICIPALITY_DANE_CODE = "Código DANE Municipio";
+    private static final String FARMER_NAME = "Nombre del Caficultor";
+
+    private final List<GeoreferenceRecordFail> errors;
+    private int row;
+
+    public GeoreferenceValidator() {
+        this.errors = new ArrayList<>();
+        this.row = 0;
+    }
+
+    public List<GeoreferenceRecordFail> validateFields(GeoreferenceRecord georeferenceRecord, GeoreferenceRequest georeferenceRequest, Department department, Municipality municipality, int row) {
+        this.row = row;
+        validateFarmerName(georeferenceRecord, georeferenceRequest);
+        validateFarmName(georeferenceRecord, georeferenceRequest);
+        validateDocumentType(georeferenceRecord, georeferenceRequest);
+        validateDocumentNumber(georeferenceRecord, georeferenceRequest);
+        validateFarmerName(georeferenceRecord, georeferenceRequest);
+        validateCultivateArea(georeferenceRecord, georeferenceRequest);
+        validateDepartment(georeferenceRecord, georeferenceRequest, department);
+        validateMunicipality(georeferenceRecord, georeferenceRequest, municipality);
+        return errors;
+    }
+
+    private void validateFarmerName(GeoreferenceRecord georeferenceRecord, GeoreferenceRequest georeferenceRequest) {
+        if (georeferenceRecord.getFarmerName().equalsIgnoreCase("")) {
+            addError(georeferenceRequest, FARMER_NAME, row, "EL NOMBRE CAFICULTOR ES REQUERIDO");
+        }
+        if (georeferenceRecord.getFarmerName().length() > 200) {
+            addError(georeferenceRequest, FARMER_NAME, row, "EL NOMBRE CAFICULTOR EXCEDE LOS 200 CARACTERES");
+        }
+    }
+
+    private void validateDocumentType(GeoreferenceRecord georeferenceRecord, GeoreferenceRequest georeferenceRequest) {
+        if (georeferenceRecord.getDocumentType().equalsIgnoreCase("") || georeferenceRecord.getDocumentType().length() > 4) {
+            addError(georeferenceRequest, "Tipo de Documento", row, "EL TIPO DE DOCUMENTO EXCEDE LA LONGITUD");
+        }
+    }
+
+    private void validateDocumentNumber(GeoreferenceRecord georeferenceRecord, GeoreferenceRequest georeferenceRequest) {
+        if (georeferenceRecord.getDocumentNumber().toString().length() > 15) {
+            addError(georeferenceRequest, "Numero de Documento", row, "EL NÚMERO DE DOCUMENTO EXCEDE LA LONGITUD");
+        }
+    }
+
+    private void validateFarmName(GeoreferenceRecord georeferenceRecord, GeoreferenceRequest georeferenceRequest) {
+        if (georeferenceRecord.getFarmName().length() > 200) {
+            addError(georeferenceRequest, "Nombre de la Finca", row, "EL NOMBRE DE LA FINCA EXCEDE LOS 200 CARACTERES");
+        }
+    }
+
+    private void validateCultivateArea(GeoreferenceRecord georeferenceRecord, GeoreferenceRequest georeferenceRequest) {
+        if (georeferenceRecord.getCultivationArea() == 0) {
+            addError(georeferenceRequest, "Área de Cultivo", row, "EL ÁREA DE CULTIVO NO CUMPLE CON EL FORMATO");
+        }
+    }
+
+    private void validateDepartment(GeoreferenceRecord georeferenceRecord, GeoreferenceRequest georeferenceRequest, Department department) {
+        if (georeferenceRecord.getDepartmentCode().equalsIgnoreCase("")) {
+            addError(georeferenceRequest, DEPARTMENT_DANE_CODE, row, "CÓDIGO DEPARTAMENTO ES REQUERIDO");
+        }
+        if (georeferenceRecord.getDepartmentCode().length() > 3) {
+            addError(georeferenceRequest, DEPARTMENT_DANE_CODE, row, "CÓDIGO DEPARTAMENTO EXCEDE LA LONGITUD");
+        }
+        if (department != null && !georeferenceRecord.getDepartmentCode().equals(department.getTxCodeDane())) {
+            addError(georeferenceRequest, DEPARTMENT_DANE_CODE, row, "EL CÓDIGO DANE DEL DEPARTAMENTO NO EXISTE");
+        }
+        if (georeferenceRecord.getDepartmentName().length() > 100) {
+            addError(georeferenceRequest, "Departamento", row, "EL NOMBRE DEL DEPARTAMENTO EXCEDE LOS 100 CARACTERES");
+        }
+    }
+
+    private void validateMunicipality(GeoreferenceRecord georeferenceRecord, GeoreferenceRequest georeferenceRequest, Municipality municipality) {
+        if (georeferenceRecord.getMunicipalityCode().equalsIgnoreCase("")) {
+            addError(georeferenceRequest, MUNICIPALITY_DANE_CODE, row, "CÓDIGO DEPARTAMENTO ES REQUERIDO");
+        }
+        if (georeferenceRecord.getMunicipalityCode().length() > 2) {
+            addError(georeferenceRequest, MUNICIPALITY_DANE_CODE, row, "CÓDIGO DEPARTAMENTO EXCEDE LA LONGITUD");
+        }
+        if (municipality != null && !georeferenceRecord.getMunicipalityCode().equals(municipality.getTxCodeDane())) {
+            addError(georeferenceRequest, MUNICIPALITY_DANE_CODE, row, "EL CÓDIGO DANE DEL MUNICIPIO NO EXISTE");
+        }
+        if (georeferenceRecord.getMunicipalityName().length() > 100) {
+            addError(georeferenceRequest, "Municipio", row, "EL NOMBRE DEL MUNICIPIO EXCEDE LOS 100 CARACTERES");
+        }
+    }
+
+    private void addError(GeoreferenceRequest georeferenceRequest, String columnName, Integer rowNumber, String errorMessage) {
+        GeoreferenceRecordFail rf = new GeoreferenceRecordFail();
+        rf.setGeoreferenceRequest(georeferenceRequest);
+        rf.setColumnName(columnName);
+        rf.setRowNumber(rowNumber);
+        rf.setErrorMessage(errorMessage);
+        errors.add(rf);
+    }
+
+}
